2dbfb4f61ddc34b4a4dc22409f24f1b3b1f3c642
Josh Justice <jjustice@bignerdranch.com>
2018-03-11 12:24:14 -0400

    Show posts in the posts route, and redirect to it

--- ember/app/routes/posts.js
+++ ember/app/routes/posts.js
@@ -1,7 +1,7 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
-  beforeModel() {
-    this.transitionTo('posts');
+  model() {
+    return this.store.findAll('post');
   }
 });
